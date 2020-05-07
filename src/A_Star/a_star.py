ba4080f0fb36a520190efb2b5e01256446ade515
Kemuel Suárez <kemuel.s21@gmail.com>
2020-05-07 15:26:52 -0400

    Merged QR code files in one with the same funtions

--- src/A_Star/a_star.py
+++ src/A_Star/a_star.py
@@ -5,7 +5,7 @@ connectionFile = "src/A_Star/Maps/SecondFloorConnections.csv"
 connections = pd.read_csv(connectionFile)
 
 nodes = pd.read_csv(nodeFile)
-print(nodeFile.loc[:,'X'])
+print(nodes.loc[:,'X'])
 print(nodes)
 print(connections)
 
