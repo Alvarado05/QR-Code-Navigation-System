c7d23a2a5e8a057ceb7ec1075e447832eb94d102
Carlos Alvarado <calvarado1458@interbayamon.edu>
2020-05-17 16:38:48 -0400

    Added a motor movement command

--- src/arduino_serial_com/sbridge.py
+++ src/arduino_serial_com/sbridge.py
@@ -6,8 +6,9 @@ ser = serial.Serial('COM5', baudrate = 9600, timeout = 1)
 
 def getSensorInfo():
     while(1):
-        ser.write(b'd\n')                   # sending a d with endline to the arduino serial
-        time.sleep(.5)                      # the frequency of the samples for the sensors to adquiere the info
+        ser.write(b'd\n')                    # sending a d with endline to the arduino serial for sensor info
+        ser.write(b'v,50,50\n')              # motor commands, v (to activate motor), 50 (left motor velocity), 50 (right motor velocity)
+        time.sleep(.01)                      # the frequency of the samples for the sensors to adquiere the info
 
         arduinoData = ser.read_until("\n").decode('ascii')        # reading the info from arduino and decoding it (ascii)
         print(arduinoData)                                        # priting the arduino serial (sensors info)
