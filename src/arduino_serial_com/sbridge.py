0986a086c205c533a5e84444e64a1366dec99295
Carlos Alvarado <cjclonjedi@gmail.com>
2020-05-17 20:25:48 -0700

    The data from sensor are now organized in dictionaries

--- src/arduino_serial_com/sbridge.py
+++ src/arduino_serial_com/sbridge.py
@@ -1,20 +1,22 @@
 import serial       # pip install serial, pip install pyserial
 import time
 
-ser = serial.Serial('COM5', baudrate = 9600, timeout = 1)   # Setup for the arduino communication
+ser = serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = 1)   # Setup for the arduino communication
 
 
 def getSensorInfo():
-    while(1):
         ser.write(b'd\n')                    # sending a d with endline to the arduino serial for sensor info
-        ser.write(b'v,50,50\n')              # motor commands, v (to activate motor), 50 (left motor velocity), 50 (right motor velocity)
-        time.sleep(.01)                      # the frequency of the samples for the sensors to adquiere the info
-
+        ser.write(b'v,50,0\n')              # motor commands, v (to activate motor), 50 (left motor velocity), 50 (right motor velocity)
+        time.sleep(.1)                      # the frequency of the samples for the sensors to adquiere the info
+        splited = []
         arduinoData = ser.read_until("\n").decode('ascii')        # reading the info from arduino and decoding it (ascii)
-        print(arduinoData)                                        # priting the arduino serial (sensors info)
-
+        arduinoData = arduinoData.splitlines()
+        for i in range(len(arduinoData)):
+                splited.append(arduinoData[i].split(','))
+        dicti = {j[0]:j[1:] for j in splited}
+        print(dicti)
   
-getSensorInfo();
+getSensorInfo()
 
 # def getValues():
     
