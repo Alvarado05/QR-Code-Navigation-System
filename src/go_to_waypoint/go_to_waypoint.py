f72ed76f064763c29bd2f2032ede63f94f5bfd69
Francisco J. Torres Cardona <picki78@gmail.com>
2020-05-21 15:46:07 -0400

    Change the hit distance

--- src/go_to_waypoint/go_to_waypoint.py
+++ src/go_to_waypoint/go_to_waypoint.py
@@ -87,7 +87,7 @@ def run(comChannel, orientations, steps, tolerance, velocity, v_decrease):
     ser = serial.Serial(str(comChannel), baudrate = 9600, timeout = .1)   # Setup for the arduino communication
     i = len(orientations)
     i2 = 0
-    hit_distance = 15
+    hit_distance = 22
     corr_angle = 0.0872665
     while i2 < i :
 
