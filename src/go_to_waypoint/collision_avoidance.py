7272036bde839dc2e03f3e278c9ca63dfd03b39d
Francisco J. Torres Cardona <picki78@gmail.com>
2020-05-21 10:40:55 -0400

    added start of logic

--- src/go_to_waypoint/collision_avoidance.py
+++ src/go_to_waypoint/collision_avoidance.py
@@ -2,11 +2,19 @@ import go_to_waypoint as gtw
 import serial
 
 ser = serial.Serial(str('/dev/ttyACM0'), baudrate = 9600, timeout = .1)   # Setup for the arduino communication
-def run(ser):
+def run(ser, hit_distance):
     data = gtw.read(ser, 121)
 
 
     print(data['USSL'][-1])
     print(data['USSR'][-1])
+    left = data['USSL'][-1]
+    right = data['USSR'][-1]
+    if left < hit_distance and right > hit_distance:
+        print("We got a hit in the left")
+    elif right < hit_distance and left > hit_distance:
+        print("We got a hit in the right")
+    else:
+        print("hit to both sides")
 
 run(ser)
