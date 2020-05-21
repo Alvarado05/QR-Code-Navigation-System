cbc8d3d68ea7d323448ccfe763da88527e9055f0
Carlos Alvarado <cjclonjedi@gmail.com>
2020-05-21 08:29:12 -0600

    Converted event to list to get the keypad values

--- src/keypad/keypadRead.py
+++ src/keypad/keypadRead.py
@@ -1,7 +1,9 @@
 from evdev import InputDevice, categorize, ecodes
 
-device = InputDevice("/dev/input/event6") # my keyboard
+device = InputDevice("/dev/input/event3") # my keyboard
 for event in device.read_loop():
     if event.type == ecodes.EV_KEY:
         print(categorize(event))
-        print(event)
+        print(type(event))
+        event = str(categorize(event))
+        print(event.split(','))
