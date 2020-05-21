6b036bc50d7f92bda57fbc4a9dc47524300a3b60
Carlos Alvarado <calvarado1458@interbayamon.edu>
2020-05-21 12:49:47 -0400

    Created a custom Buzzer Library

---
+++ arduino/libraries/Buzzer/Buzzer.h
@@ -0,0 +1,17 @@
+#ifndef Buzzer_h
+#define Buzzer_h
+
+class Buzzer
+{
+    public:
+    //Constructors
+    Buzzer(byte buzzerPin)
+
+    //Functions
+    void arrived();
+    void rotate();
+    void error();
+
+};
+
+#endif
