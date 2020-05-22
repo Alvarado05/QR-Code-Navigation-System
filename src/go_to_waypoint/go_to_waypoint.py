5e9263fe87799fd4666b6932361850fab6485f05
Francisco J. Torres Cardona <picki78@gmail.com>
2020-05-21 21:20:22 -0400

    Nothing important

--- src/go_to_waypoint/go_to_waypoint.py
+++ src/go_to_waypoint/go_to_waypoint.py
@@ -11,14 +11,10 @@
 import serial       # pip install serial, pip install pyserial
 import time
 from qr_code import QRCodeFunctions as qrf
-from go_to_waypoint import collision_avoidance as cav
 import math
 
 def move(ser, leftV, rightV):
-    print("LeftV:", leftV)
-    print("rightV:", rightV)
     concatenation = 'v,'+ str(leftV) + ',' + str(rightV) +'\n'
-    print(concatenation)
     ser.write(bytes(concatenation, 'ascii')) 
 
 def stop(ser):
@@ -29,26 +25,13 @@ def read(ser, slp):
     splitedList = []                                        # the frequency of the samples for the sensors to adquiere the info
     arduinoData = ser.read_until("\n").decode('ascii')      # reading the info from arduino and decoding it (ascii)
     arduinoData = arduinoData.splitlines()
-    
     for i in range(len(arduinoData)):
             splitedList.append(arduinoData[i].split(','))   # priting the arduino serial (sensors info)
     print(splitedList)
     dataDict = {j[0]:[float(i) for i in j[1:]]  for j in splitedList}
     return dataDict                                         # priting the arduino serial (sensors info)
 
-def checkRange(degree):
-    changeValue = False
-    if degree  < 0:
-        degree = degree + (2*math.pi)
-        print("changed Value")
-        changeValue = True
-    elif degree > (2 * math.pi):
-        degree = degree - (2*math.pi)
-        print("changed Value")
-        changeValue = True
-    return degree , changeValue
-
-def changeOrientation(ser, velocity, start_orientation, final_orientation):
+def alignOrientation(ser, velocity, start_orientation, final_orientation):
     angle_between = start_orientation-final_orientation
     if angle_between < 0:
         angle_between = angle_between + 2*math.pi
@@ -58,67 +41,58 @@ def changeOrientation(ser, velocity, start_orientation, final_orientation):
         move(ser,velocity*(-1),velocity)
     return None
 
-def alignOrientation (ser, velocity, final_orientation, tolerance, v_decrease):
-    # while orientation is not right, rotate
-    data = read(ser, .0001)
-    cur_orientation = data['IMU'][-1]
-
-    min_orientation = final_orientation - tolerance
-    max_orientation = final_orientation + tolerance
-
-    velocity = velocity*v_decrease
-    # if any of the two fall outside the rango of 0-360, convert them
-    min_orientation, changeValue = checkRange(min_orientation)
-    max_orientation, changeValue = checkRange(max_orientation)
-    print("The minimum orientation is:", min_orientation)
-    print("The maximum orientation is:", max_orientation)
-
-    if changeValue == False:
-        while (cur_orientation <= (min_orientation) or cur_orientation >= (max_orientation)):
-            data = read(ser, .0001)
-            cur_orientation = data['IMU'][-1]
-            print("Aligning from:", cur_orientation, "to:", final_orientation)
-            changeOrientation(ser, velocity, cur_orientation, final_orientation)
-            time.sleep(.2)
-            print("changeValue:", changeValue)
-            print(cur_orientation)
-    else: 
-        while (cur_orientation > max_orientation and cur_orientation < min_orientation):
-            data = read(ser, .0001)
-            cur_orientation = data['IMU'][-1]
-            changeOrientation(ser, velocity, cur_orientation, final_orientation) 
-            print("changeValue:", changeValue)          
-            print(cur_orientation)
-    print('Finished Rotation')        
-    stop(ser)
-
-def run(comChannel, orientations, steps, tolerance, velocity, v_decrease):
+def run(comChannel, orientations, steps, tolerance, velocity):
     ser = serial.Serial(str(comChannel), baudrate = 9600, timeout = .1)   # Setup for the arduino communication
     i = len(orientations)
     i2 = 0
-    hit_distance = 24
-    front_hit_distance = 40
-    corr_angle = 0.174533
     while i2 < i :
 
         print("Orientation:", orientations[i2])
         print("Step:", steps[i2])
+        min_orientation = orientations[i2] - tolerance
+        max_orientation = orientations[i2] + tolerance
+
+        changeValue = False
+        # if any of the two fall outside the rango of 0-360, convert them
+        if min_orientation  < 0:
+            min_orientation = min_orientation + (2*math.pi)
+            changeValue = True
+        elif max_orientation > 2*math.pi:
+            max_orientation = max_orientation - (2*math.pi)
+            changeValue = True
+
+
+        data = read(ser, .0001)
+        cur_orientation = data['IMU'][-1]
+
+        print("Cur_orientation: ", cur_orientation)
+
+        # while orientation is not right, rotate
+        if changeValue == False:
+            while (cur_orientation <= (min_orientation) or cur_orientation >= (max_orientation)):
+                print("Aligning from:", cur_orientation, "to:", orientations[i2])
+                alignOrientation(ser, velocity, cur_orientation, orientations[i2])
+                data = read(ser, .0001)
+                cur_orientation = data['IMU'][-1]
+                print(cur_orientation)
+        else: 
+            while (cur_orientation > max_orientation and cur_orientation < min_orientation):
+                alignOrientation(ser, velocity, cur_orientation, orientations[i2])
+                data = read(ser, .0001)
+                cur_orientation = data['IMU'][-1]
+                print(cur_orientation)
+        print('Finished Rotation')        
+        stop(ser)
         
-
-        alignOrientation(ser, velocity, orientations[i2], tolerance, v_decrease)
-
         scan = qrf.qrScanner()                              # scan qrCode
         
         while scan == None or scan != steps[i2]:                                 # while qrcode not present
-            cav.run(ser, hit_distance, front_hit_distance, corr_angle, velocity, tolerance, v_decrease)
             move(ser,velocity, velocity)                    #   Move forward
-            print("Scanning")
             scan = qrf.qrScanner()                          #   scan qrCode
             if scan != None:
                 scan = int(scan)
         stop(ser)                                           # stop
         i2 = i2 + 1
-   
     return None
 
 
