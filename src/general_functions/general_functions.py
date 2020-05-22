ddef7abcbf36007cddb9e1b5ac517f8f96f7a62e
Carlos Alvarado <calvarado1458@interbayamon.edu>
2020-05-22 04:32:13 -0400

    Fixed keypad in gf

--- src/general_functions/general_functions.py
+++ src/general_functions/general_functions.py
@@ -71,26 +71,28 @@ def northToCardinal(north):
 
 def keypadRead():
     device = InputDevice("/dev/input/event3") # my keyboard
-    for event in device.read_loop():
-        if event.type == ecodes.EV_KEY:
-            event = str(categorize(event))
-            event = event.split(',')
-            event = event[1].split('KP')
-            event = event[1]
-            event = event[:-1]
 
-            if (event == 'ASTERISK'):
-                event = '*'
-            elif(event == 'SLASH'):
-                event = '/'
-            elif(event == 'PLUS'):
-                event = '+'
-            elif(event == 'MINUS'):
-                event = '-'
-            elif(event == 'DOT'):
-                event = '.'
-            elif(event == 'ENTER'):
-                event = '\n'
-            else:
-                event = event
-    return event
+    event = device.active_keys(verbose=True)
+
+    while event == []:
+        event = device.active_keys(verbose=True)
+
+    device.close()
+    event = event[0][0].split('KP')
+    event = event[1]
+
+    if (event == 'ASTERISK'):
+        event = '*'
+    elif(event == 'SLASH'):
+        event = '/'
+    elif(event == 'PLUS'):
+        event = '+'
+    elif(event == 'MINUS'):
+        event = '-'
+    elif(event == 'DOT'):
+        event = '.'
+    elif(event == 'ENTER'):
+        event = '\n'
+    else:
+        event = event
+return event
